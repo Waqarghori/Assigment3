eb7439a79446ea1b84b96062c15d44dc1cb7582a
Waqarghori <waqar.iqbal789@gmail.com>
2024-12-21 01:26:57 +0500

    Update

---
+++ app.js
@@ -0,0 +1,44 @@
+//=====Chapter No 9-10=====
+
+
+//=====Chapter No 9-10 (Q.1=====
+// let city = prompt("enter your city name");
+// if (city == "karachi"){
+//     console.log("welcome to city of light");
+// }
+
+//=====Chapter No 9-10 (Q.2=====
+// let gander = prompt("enter your gender");
+// if (gander === "male"){
+//     console.log("Good Morning Sir")
+// }
+// if (gander === "female"){
+//    console.log("Good Morning Ma’am")
+// }
+
+
+//=====Chapter No 9-10 (Q.3=====
+// let trafficSignal = prompt("select traffic signal color","red yellow or green");
+// if (trafficSignal === "red"){
+//     console.log("must stope")
+// }
+// if (trafficSignal === "yellow"){
+//     console.log("ready to move")
+// }
+// if (trafficSignal === "green"){
+//     console.log("move now")
+// }
+
+//=====Chapter No 9-10 (Q.4====
+// let fuelInTank = prompt("enter your remaining fuel in litres");
+// if (fuelInTank < 0.25){
+//     console.log("Please refill the fuel in your car")
+// }
+// else{
+//     console.log("You have enough fuel for now");
+// }
+
+//=====Chapter No 9-10 (Q.5=====
+
+
+
