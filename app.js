f0d67ff994a44c5e2141f27cf8648baea9e1a931
Waqarghori <waqar.iqbal789@gmail.com>
2024-12-22 20:13:56 +0500

    Updated

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-//=====Chapter No 9-10=====
+//=====Chapter No 6-9=====
 
+//=====Chapter No 6-9 (Q.2)=====
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write("The resul id : " + result);
 
-//=====Chapter No 9-10 (Q.1=====
+//=====Chapter No 6-9 (Q.3)=====
+// let user = prompt("enter your name");
+// alert("Welcome " + user);
+
+//=====Chapter No 6-9 (Q.6)=====
+let subject1 = +prompt("englih");
+let subject2 = +prompt("math");
+let subject3 = +prompt("urdu");
+let totalNum = (100);
+document.write(`<h1>Total Marks & percentage:</h1>`)
+document.write(`english &nbsp &nbsp ${totalNum} &nbsp &nbsp ${subject1} &nbsp &nbsp ${subject1 * 100 / totalNum}%</br>`);
+document.write(`math &nbsp &nbsp ${totalNum} &nbsp &nbsp ${subject2} &nbsp &nbsp ${subject2 * 100 / totalNum}%</br>`);
+document.write(`urdu &nbsp &nbsp ${totalNum} &nbsp &nbsp ${subject3} &nbsp &nbsp ${subject3 * 100 / totalNum}%</br>`);
+let totalMarks = (totalNum+totalNum+totalNum);
+let getedNum = (subject1+subject2+subject3);
+let totalPercentage = (getedNum * 100 / totalMarks);
+document.write(`&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp${totalMarks} &nbsp ${getedNum} &nbsp ${totalPercentage}%`);
+//=====Chapter No 9-11 =====
+
+//=====Chapter No 9-11 (Q.1)=====
 // let city = prompt("enter your city name");
 // if (city == "karachi"){
 //     console.log("welcome to city of light");
 // }
 
-//=====Chapter No 9-10 (Q.2=====
+//=====Chapter No 9-11 (Q.2=====
 // let gander = prompt("enter your gender");
 // if (gander === "male"){
 //     console.log("Good Morning Sir")
@@ -17,7 +40,8 @@
 // }
 
 
-//=====Chapter No 9-10 (Q.3=====
+
+//=====Chapter No 9-11 (Q.3=====
 // let trafficSignal = prompt("select traffic signal color","red yellow or green");
 // if (trafficSignal === "red"){
 //     console.log("must stope")
@@ -29,7 +53,7 @@
 //     console.log("move now")
 // }
 
-//=====Chapter No 9-10 (Q.4====
+//=====Chapter No 9-11 (Q.4====
 // let fuelInTank = prompt("enter your remaining fuel in litres");
 // if (fuelInTank < 0.25){
 //     console.log("Please refill the fuel in your car")
@@ -38,7 +62,7 @@
 //     console.log("You have enough fuel for now");
 // }
 
-//=====Chapter No 9-10 (Q.5=====
+//=====Chapter No 9-11 (Q.5=====
 
 
 
